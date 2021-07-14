69b63707573caded8bd340f2dda7add2c0351103
swayamshreemohanty <mohantyswayamshree@gmail.com>
2021-07-14 23:56:41 +0530

    String tut. continue

---
+++ Strings/Tut.cpp
@@ -0,0 +1,61 @@
+#include"iostream"
+
+using namespace std;
+
+int main()
+{
+    // string str;          //String declaration     
+    // cin>>str;            //Take the complete sentence as input but without space
+    // string str (cin>>)="Swayamshree";    //cout="Swayamshree"
+    // string str (cin>>)="Swayamshree Mohanty";    //cout="Swayamshree"
+    // getline(cin,str);    //Take the complete sentence as input. 
+    // string str(5, 'n');  //print "n" 5 times
+// ***********************************
+    // string s1="fam";
+    // string s2="ily";
+    // cout<<s1+s2;
+// ***********************************
+    // string s1="fam";
+    // string s2="ily";
+    // s1.append(s2);
+    // cout<<s1;
+//*************************************
+    // string str="Swayamshree";
+    // cout<<str[2];
+//*************************************
+    // string str="Swayamshree Mohanty";        //size() and length() is used to determind how many elements present in the string.
+    // for (int i = 0; i < str.length(); i++)
+    // {
+    //     cout<<str[i];    //Swayasmhree Mohanty
+    // }
+//**************************************
+    // string abc="Swayamshree Mohanty";
+    // cout<<abc;  //cout=Swayamshree Mohanty
+    // abc.clear();
+    // cout<<abc;  //cout=" "
+//**************************************
+    // string s1="abc";
+    // string s2="xbc";
+    // if (!s1.compare(s2))
+    // {
+    //     cout<<"Two strings are same";
+    // }
+    // else
+    // {
+    //     cout<<"Two strings are not same";
+    // }
+//***************************************
+    // string s1="abc";
+    // s1.clear();         // clear() is used to clear the string from the variable/identifier
+    // if (s1.empty())
+    // {
+    //     cout<<"This string is empty";
+    // }
+    // else
+    // {
+    //     cout<<"This string is not empty";
+    // }
+    
+//***************************************
+    return 0;
+}
