fd83386f3e061c704ce9ecae8a1e601eb592fae2
swayamshreemohanty <mohantyswayamshree@gmail.com>
2021-07-20 20:13:50 +0530

    String tut ended

--- Strings/Tut.cpp
+++ Strings/Tut.cpp
@@ -1,5 +1,6 @@
 #include"iostream"
-
+#include"string"
+#include"algorithm" //is used for sort(string.start(), string.end())
 using namespace std;
 
 int main()
@@ -8,8 +9,9 @@ int main()
     // cin>>str;            //Take the complete sentence as input but without space
     // string str (cin>>)="Swayamshree";    //cout="Swayamshree"
     // string str (cin>>)="Swayamshree Mohanty";    //cout="Swayamshree"
-    // getline(cin,str);    //Take the complete sentence as input. 
-    // string str(5, 'n');  //print "n" 5 times
+
+    // getline(cin,str);                        //Take the complete sentence as input. 
+    // string str(5, 'n');                      //print "n" 5 times
 // ***********************************
     // string s1="fam";
     // string s2="ily";
@@ -17,7 +19,7 @@ int main()
 // ***********************************
     // string s1="fam";
     // string s2="ily";
-    // s1.append(s2);
+    // s1.append(s2);                           //append() is used to combine two strings as per user choice
     // cout<<s1;
 //*************************************
     // string str="Swayamshree";
@@ -31,12 +33,12 @@ int main()
 //**************************************
     // string abc="Swayamshree Mohanty";
     // cout<<abc;  //cout=Swayamshree Mohanty
-    // abc.clear();
+    // abc.clear();                             //clear() is used to clear the string from the identifier
     // cout<<abc;  //cout=" "
 //**************************************
     // string s1="abc";
     // string s2="xbc";
-    // if (!s1.compare(s2))
+    // if (!s1.compare(s2))                     //compare() is used to find the greater and smaller string from two strings 
     // {
     //     cout<<"Two strings are same";
     // }
@@ -57,5 +59,40 @@ int main()
     // }
     
 //***************************************
+
+    // string s1="Swayamshree Mohanty";
+    
+    // s1.erase(3,3);           //erase(from_where, how_many) is used to remove the elements
+    // cout<<s1;
+
+//****************************************
+
+    // string s1="Swayamshree";
+
+    // cout<<s1.find("ree");       //find("element_in_the_string")  is used to find the element
+
+//****************************************
+    // string s1="abef";
+    // cout<<s1.insert(2,"cd");        //insert(position,"Element want to add") is used to insert element at desire index
+
+//****************************************
+    // string s1="Swayamshree";
+    // cout<<s1.substr(0,6);            // substr(start_index, no_of_elements), is used to extract the elements from a whole string
+
+//****************************************
+    // string s="420";
+    // int x= stoi(s);
+    // cout<<x;
+//*****************************************
+
+    // int x=420;
+    // cout<<to_string(x)+"2";
+//******************************************
+
+    // string s="dbcaef";
+    // sort(s.begin(), s.end());   //sort(string.begin(), string.end()), here begin() and end() is used to identify the start and end iterator of the string
+    // cout<<s;
+
+
     return 0;
 }
